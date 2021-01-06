176abd0c809ab3283b82fc35784753d2caf59d26
Matt Lebrun <matt@lebrun.org>
2021-01-06 15:34:01 +0800

    Update pdftk-ubuntu18.04-amd64.sh
    Removed sudo for docker installation

--- pdftk-ubuntu18.04-amd64.sh
+++ pdftk-ubuntu18.04-amd64.sh
@@ -12,19 +12,19 @@ wget http://launchpadlibrarian.net/340410966/libgcj17_6.4.0-8ubuntu1_amd64.deb \
  http://launchpadlibrarian.net/337429932/libgcj-common_6.4-3ubuntu1_all.deb \
  https://launchpad.net/ubuntu/+source/pdftk/2.02-4build1/+build/10581759/+files/pdftk_2.02-4build1_amd64.deb \
  https://launchpad.net/ubuntu/+source/pdftk/2.02-4build1/+build/10581759/+files/pdftk-dbg_2.02-4build1_amd64.deb
-
-
 echo -e "Packages for pdftk downloaded\n\n"
+
 # install packages 
 echo -e "\n\n Installing pdftk: \n\n"
-sudo apt-get install ./libgcj17_6.4.0-8ubuntu1_amd64.deb \
+apt-get install ./libgcj17_6.4.0-8ubuntu1_amd64.deb \
     ./libgcj-common_6.4-3ubuntu1_all.deb \
     ./pdftk_2.02-4build1_amd64.deb \
     ./pdftk-dbg_2.02-4build1_amd64.deb
 echo -e "\n\n pdftk installed\n"
 echo -e "   try it in shell with: > pdftk \n"
+
 # delete deb files in /tmp directory
-rm ./libgcj17_6.4.0-8ubuntu1_amd64.deb
-rm ./libgcj-common_6.4-3ubuntu1_all.deb
-rm ./pdftk_2.02-4build1_amd64.deb
-rm ./pdftk-dbg_2.02-4build1_amd64.deb
+rm /tmp/libgcj17_6.4.0-8ubuntu1_amd64.deb
+rm /tmp/libgcj-common_6.4-3ubuntu1_all.deb
+rm /tmp/pdftk_2.02-4build1_amd64.deb
+rm /tmp/pdftk-dbg_2.02-4build1_amd64.deb
